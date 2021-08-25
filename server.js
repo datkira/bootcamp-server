434ea44245c7b8f0f0390813c78e811add92ee7e
Nguyen Tien Dat <abc.nguyentiendat@gmail.com>
2021-08-26 02:32:56 +0700

    add logger

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 
 // Route files
 const bootcamp = require('./routes/bootcamp')
 dotenv.config({ path: './config/config.env' })
 const app = express()
+
+// Logging middleware - Dev
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 // Mount routers
 app.use('/api/v1/bootcamp/', bootcamp)
